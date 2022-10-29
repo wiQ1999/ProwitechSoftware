173ea4a21e3f4e32ff5e9372eae93d05be0faa88
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-10-29 16:05:12 +0200

    Dodanie modelu rezultatu zapytania o logowanie, aktualziacja handlera

--- Application/Authentication/Queries/Handlers/LoginQueryHandler.cs
+++ Application/Authentication/Queries/Handlers/LoginQueryHandler.cs
@@ -1,41 +1,57 @@
-﻿using Application.Authentication.Queries.Requests;
+﻿using Application.Authentication.DTOs;
+using Application.Authentication.Queries.Requests;
 using Application.Interfaces.Services;
+using Application.Users.DTOs;
+using AutoMapper;
 using Infrastructure.Interfaces.Repositories;
 using MediatR;
 
 namespace Application.Authentication.Queries.Handlers;
 
 public class LoginQueryHandler
-    : IRequestHandler<LoginQuery, string?>
+    : IRequestHandler<LoginQuery, AuthenticationResponse>
 {
     private readonly IUsersRepository _usersRepository;
     private readonly IPermissionsSelector _permissionsSelector;
+    private readonly IMapper _mapper;
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
     public LoginQueryHandler(
         IUsersRepository usersRepository, 
         IPermissionsSelector permissionsSelector, 
+        IMapper mapper,
         IJwtTokenGenerator jwtTokenGenerator)
     {
         _usersRepository = usersRepository;
         _permissionsSelector = permissionsSelector;
+        _mapper = mapper;
         _jwtTokenGenerator = jwtTokenGenerator;
     }
 
-    public async Task<string?> Handle(
+    public async Task<AuthenticationResponse> Handle(
         LoginQuery request, CancellationToken cancellationToken)
     {
         var user = await _usersRepository.GetUserByLoginAndPasswordAsync(
             request.Login, request.Password, cancellationToken);
 
         if (user == null)
-            return null!;
-        
+            return new AuthenticationResponse
+            {
+                Token = null,
+                IsGenerated = false
+            };
+
+        var mappedUser = _mapper.Map<UserDto>(user);
+
         var permissions = await _permissionsSelector.GetCompleteUserAndRolePermissions(
             user.Id, cancellationToken);
 
-        var token = _jwtTokenGenerator.GenerateToken(user, permissions);
+        var token = _jwtTokenGenerator.GenerateToken(mappedUser, permissions);
 
-        return token;
+        return new AuthenticationResponse
+        {
+            Token = token,
+            IsGenerated = !string.IsNullOrEmpty(token)
+        };
     }
 }
--- Application/Authentication/Queries/Requests/LoginQuery.cs
+++ Application/Authentication/Queries/Requests/LoginQuery.cs
@@ -1,9 +1,10 @@
-﻿using MediatR;
+﻿using Application.Authentication.DTOs;
+using MediatR;
 using System.ComponentModel.DataAnnotations;
 
 namespace Application.Authentication.Queries.Requests;
 
-public record LoginQuery : IRequest<string>
+public record LoginQuery : IRequest<AuthenticationResponse>
 {
     [Required]
     public string Login { get; set; } = null!;
