1b4a8e9e3ffb4440141fe993808e5595c468ad71
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-10-30 13:54:53 +0100

    Dodanie filtra i atrybutu dla weryfikacji uprawnień do zasobu

---
+++ Web/Authorization/SourceAuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Web.Authorization;
+
+namespace Web.AuthorizeAttributes;
+
+public class SourcePermissionsAttribute : TypeFilterAttribute
+{
+    public SourcePermissionsAttribute(
+        AppSource source, PermissionProperty permissionProeprty) 
+        : base(typeof(SourcePermissionsFilter))
+    {
+        Arguments = new object[]
+        {
+            CreateClaim(source, permissionProeprty)
+        };
+    }
+
+    public SourcePermissionsAttribute(
+        AppSource source, params PermissionProperty[] permissionProeprties)
+        : base(typeof(SourcePermissionsFilter))
+    {
+        var claims = permissionProeprties.Select(pp => CreateClaim(source, pp));
+        Arguments = claims.ToArray<object>();
+    }
+
+    private Claim CreateClaim(
+        AppSource source, PermissionProperty permissionProeprty)
+        => new("Permissions", $"{source}_{permissionProeprty}");
+}
---
+++ Web/Authorization/SourcePermissionsFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace Web.Authorization;
+
+public class SourcePermissionsFilter : IAuthorizationFilter
+{
+    private readonly Claim _claim;
+
+	public SourcePermissionsFilter(Claim claim)
+	{
+		_claim = claim;
+	}
+
+	public void OnAuthorization(AuthorizationFilterContext context)
+	{
+        var hasClaim = context.HttpContext.User.Claims.Any(c
+            => c.Type == _claim.Type && c.Value == _claim.Value);
+
+        if (!hasClaim)
+            context.Result = new ForbidResult();
+    }
+}
