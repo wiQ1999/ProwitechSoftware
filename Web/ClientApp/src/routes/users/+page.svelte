4a70756c7131611f67dff53b01a8e4ea406f974e
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-11-17 23:19:10 +0100

    Dodanie tabeli bazowej wszystkich użytkowników

--- Web/ClientApp/src/routes/users/+page.svelte
+++ Web/ClientApp/src/routes/users/+page.svelte
@@ -2,14 +2,51 @@
     import { onMount } from "svelte";
     import { getAllUsers } from "../../stores/Users";
 
-    let roles = [];
-
+    let users = [];
     onMount(async () => {
-        roles = await getAllUsers();
+        users = await getAllUsers();
     });
+
+    function edit(id) {
+        window.location.href = `/roles/details/${id}`;
+    }
+
+    function deletePopUp(user) {
+        console.log(user);
+    }
 </script>
 
-<h1>TEST</h1>
-{#each roles as role}
-    <p>{role.firstName}</p>
-{/each}
+<br />
+
+<table>
+    <tr>
+        <th>Imię</th>
+        <th>Nazwisko</th>
+        <th>Email</th>
+        <th>Rola</th>
+    </tr>
+    {#each users as user}
+        <tr on:dblclick|preventDefault={() => edit(user.id)}>
+            <td>{user.firstName ?? ""}</td>
+            <td>{user.lastName ?? ""}</td>
+            <td>{user.email ?? ""}</td>
+            <td>{user.role.name ?? ""}</td>
+            <td>
+                <button on:click|preventDefault={() => edit(user.id)}>
+                    Szczegóły
+                </button>
+            </td>
+            <td>
+                <button on:click|preventDefault={() => deletePopUp(user)}>
+                    Usuń
+                </button>
+            </td>
+        </tr>
+    {/each}
+</table>
+
+<style>
+    td {
+        border-bottom: 2px solid;
+    }
+</style>
