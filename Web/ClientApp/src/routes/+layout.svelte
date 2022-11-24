b6592931276e7b09bbf06e1fd15b4b8ca8b15f34
HubertoNowako <hubinho322@gmail.com>
2022-11-24 22:02:17 +0100

    poprawiony wygląd nava

--- Web/ClientApp/src/routes/+layout.svelte
+++ Web/ClientApp/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <header>
   <nav>
     <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/settings">Settings</a>
+    <a href="/about">O nas</a>
+    <a href="/settings">Ustawienia</a>
+    <a href="/Building">Budynki</a>
+    <a href="/PropertyManager">Zarządcy</a>
   </nav>
 </header>
 <main>
@@ -15,5 +17,29 @@
   :global(body) {
     background-color: white;
     color: black;
+  }
+
+  header, main{
+    padding-top:4%;
+  }
+
+  nav{
+    text-align: center;
+  }
+
+  a{
+    text-decoration: none;
+    font-family:'Brush Script MT', cursive;
+    color: black;
+    padding: 3% 5%;
+    margin: 0 1%;
+    border: 2px solid black;
+    background-color: #b4e8a5;
+    border-radius: 20px;
+    
+  }
+
+  a:hover{
+    background-color: #91e07b;
   }
 </style>
--- Web/ClientApp/src/routes/+page.svelte
+++ Web/ClientApp/src/routes/+page.svelte
@@ -8,3 +8,20 @@
     <!-- elementy strony -->
   </slot>
 </main>
+
+<style>
+  nav{
+    text-align: center;
+    margin-top: 4%;
+  }
+
+  a{
+    text-decoration: none;
+    font-family:'Brush Script MT', cursive;
+    padding: 3% 5%;
+    margin: 0 1%;
+    border: 2px solid black;
+    background-color: #b4e8a5;
+    border-radius: 20px;
+  }
+</style>
