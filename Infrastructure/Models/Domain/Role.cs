516529c5fbfd6cab273b61543b8bcf00bd41e4ae
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-09 00:05:53 +0100

    Refaktoryzacja modeli bazodawnowych

--- Infrastructure/Models/Domain/Role.cs
+++ Infrastructure/Models/Domain/Role.cs
@@ -5,6 +5,7 @@ namespace Infrastructure.Models.Domain;
 public class Role : BaseEntity
 {
     public string Name { get; set; } = default!;
-    public ICollection<User> Users { get; set; } = default!;
-    public ICollection<Permission> Permissions { get; set; } = default!;
+
+    public virtual ICollection<User> Users { get; set; } = default!;
+    public virtual ICollection<Permission> Permissions { get; set; } = default!;
 }
