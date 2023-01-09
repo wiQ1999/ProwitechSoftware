176593b471751469c66189fac4edae53fef902d8
hannahfili <hannahfilipowska@gmail.com>
2023-01-09 16:52:35 +0100

    małe zmiany w wyglądzie pop-upów

--- Web/ClientApp/src/lib/components/ShowPropertyManagerPopUp.svelte
+++ Web/ClientApp/src/lib/components/ShowPropertyManagerPopUp.svelte
@@ -25,34 +25,43 @@
   //     }
   //   }
   // }
-export let PropertyManagerDTO;
+  export let PropertyManagerDTO;
   export let message;
   console.log(PropertyManagerDTO);
 </script>
-<div class="w-[50%] h-[50%] text-center relative my-[10%] mx-auto bg-white rounded-2xl pt-10 px-2">
+
+<div
+  class="w-[50%] h-[50%] text-center relative my-[10%] mx-auto bg-white rounded-2xl pt-10 px-2"
+>
   {#if PropertyManagerDTO == null}
     Nie udało się odczytać danych Zarządcy Nieruchomości.
   {:else}
-    <p>{message} </p>
-    <p>Imię i nazwisko: <span class="font-semibold">{PropertyManagerDTO.name}</span></p>
-    Adres: 
+    <p>{message}</p>
+    <p>
+      Imię i nazwisko: <span class="font-semibold"
+        >{PropertyManagerDTO.name}</span
+      >
+    </p>
+    Adres:
     <span class="font-semibold">
-    {#if PropertyManagerDTO.fullAddress.buildingAddress.postalCode != null}
-      {PropertyManagerDTO.fullAddress.buildingAddress.postalCode}
-    {/if}
-    {PropertyManagerDTO.fullAddress.buildingAddress.cityName}, 
-    {PropertyManagerDTO.fullAddress.buildingAddress.streetName}
-    {PropertyManagerDTO.fullAddress.buildingAddress.buildingNumber}
-    {#if PropertyManagerDTO.fullAddress.localNumber != ""}
-    /{PropertyManagerDTO.fullAddress.localNumber} 
-    {/if}
-    {#if PropertyManagerDTO.fullAddress.staircaseNumber != ""}
-      klatka {PropertyManagerDTO.fullAddress.staircaseNumber}
-    {/if} </span>
+      {#if PropertyManagerDTO.fullAddress.buildingAddress.postalCode != null}
+        {PropertyManagerDTO.fullAddress.buildingAddress.postalCode}
+      {/if}
+      {PropertyManagerDTO.fullAddress.buildingAddress.cityName},
+      {PropertyManagerDTO.fullAddress.buildingAddress.streetName}
+      {PropertyManagerDTO.fullAddress.buildingAddress.buildingNumber}
+      {#if PropertyManagerDTO.fullAddress.propertyAddress.venueNumber != ""}
+        m. {PropertyManagerDTO.fullAddress.propertyAddress.venueNumber}
+      {/if}
+      {#if PropertyManagerDTO.fullAddress.propertyAddress.staircaseNumber != ""}
+        klatka {PropertyManagerDTO.fullAddress.propertyAddress.staircaseNumber}
+      {/if}
+    </span>
     <br />
-     <p>Nr telefonu:
+    <p>
+      Nr telefonu:
       <span class="font-semibold">{PropertyManagerDTO.phoneNumber}</span>
-  </p>
-    <br>
+    </p>
+    <br />
   {/if}
-  </div>
+</div>
