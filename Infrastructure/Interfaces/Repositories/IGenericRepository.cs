86f0572647951e913a2c0c3eb780f2a0b8f4d819
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-10 22:45:43 +0100

    Dodanie implementacji generycznego repozytorium

---
+++ Infrastructure/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Interfaces.Repositories;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+
+    Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task InsertAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task UpdateAsync(TEntity entityToUpdate, CancellationToken cancellationToken);
+
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Database;
+using Infrastructure.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    internal ProwitechDbContext Context;
+    internal DbSet<TEntity> DbSet;
+
+    public GenericRepository(ProwitechDbContext context)
+    {
+        Context = context;
+        DbSet = context.Set<TEntity>();
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await DbSet.ToArrayAsync(cancellationToken);
+    }
+
+    public virtual async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return (await DbSet.FindAsync(id, cancellationToken))!;
+    }
+
+    public virtual async Task InsertAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await DbSet.AddAsync(entity, cancellationToken);
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        DbSet.Attach(entity);
+        Context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public virtual async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        TEntity entity = await GetByIdAsync(id, cancellationToken);
+
+        if (Context.Entry(entity).State == EntityState.Detached)
+            DbSet.Attach(entity);
+
+        DbSet.Remove(entity);
+    }
+}
