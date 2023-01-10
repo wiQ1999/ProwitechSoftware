826f1b611faf70766e8f400ea4678d71ed2c04ce
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-10 22:45:56 +0100

    Dokdanie interfejsu wzorca UnitOfWork

---
+++ Infrastructure/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Interfaces.Repositories;
+using Infrastructure.Models.Domain;
+
+namespace Infrastructure.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<Role> RolesRepository { get; }
+
+    void Save();
+}
