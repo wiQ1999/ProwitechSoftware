7db06d44809fde5cfa4c9b26c5e85f4b984bc7ed
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-11 00:00:46 +0100

    Dodanie implementacji interfejsu IUnitOfWork i zmiana lokalizacji

---
+++ Infrastructure/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models.Domain;
+
+namespace Infrastructure.Interfaces.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<Role> RolesRepository { get; }
+
+    void Save();
+}
---
+++ Infrastructure/Repositories/UniteOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Database;
+using Infrastructure.Interfaces.Repositories;
+using Infrastructure.Models.Domain;
+
+namespace Infrastructure.Repositories;
+
+public class UniteOfWork : IUnitOfWork
+{
+    private ProwitechDbContext _context;
+    private IGenericRepository<Role>? _rolesRepository;
+    private bool disposed = false;
+
+    public IGenericRepository<Role> RolesRepository
+        => _rolesRepository ??= new GenericRepository<Role>(_context);
+
+    public UniteOfWork(ProwitechDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Save()
+        => _context.SaveChanges();
+
+    public void Dispose()
+    {
+        if (disposed)
+            return;
+
+        _context.Dispose();
+        disposed = true;
+    }
+}
