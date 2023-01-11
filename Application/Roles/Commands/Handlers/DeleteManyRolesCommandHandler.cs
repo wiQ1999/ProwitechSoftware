1823ee19ae9791eee042a4f98b06033f092798ec
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-12 00:00:14 +0100

    Dodanie endpointu usuwania wielu ról i jego obsługa

---
+++ Application/Roles/Commands/Handlers/DeleteManyRolesCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Roles.Commands.Requests;
+using Infrastructure.Interfaces.Repositories;
+using MediatR;
+
+namespace Application.Roles.Commands.Handlers;
+
+public class DeleteManyRolesCommandHandler : IRequestHandler<DeleteManyRolesCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteManyRolesCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(DeleteManyRolesCommand request, CancellationToken cancellationToken)
+    {
+        foreach (var id in request.Ids)
+            await _unitOfWork.RolesRepository.DeleteAsync(id, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
