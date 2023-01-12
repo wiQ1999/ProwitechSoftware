76815c149d944d8247e91b571a2b9f1ceb5b6b18
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-13 00:25:46 +0100

    Dodanie endpointu usuwania wielu użytkowników

---
+++ Application/Users/Commands/Handlers/DeleteManyUsersCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Users.Commands.Requests;
+using Infrastructure.Interfaces.Repositories;
+using MediatR;
+
+namespace Application.Roles.Commands.Handlers;
+
+public class DeleteManyUsersCommandHandler : IRequestHandler<DeleteManyUsersCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteManyUsersCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(DeleteManyUsersCommand request, CancellationToken cancellationToken)
+    {
+        foreach (var id in request.Ids)
+            await _unitOfWork.UsersRepository.DeleteAsync(id, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
