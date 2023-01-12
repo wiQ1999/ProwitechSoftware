d64f1edd526cda5f1be2b4b772e7c45dc8e65ac8
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-12 18:54:30 +0100

    Dodanie nowych typów wyjątków

---
+++ Infrastructure/Models/Exceptions/InvalidLengthException.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Common.Helpers;
+using Infrastructure.Models.Enums;
+
+namespace Infrastructure.Models.Exceptions;
+
+public class InvalidLengthException : Exception
+{
+	public InvalidLengthException(AppSource source, string propertyName, int maximuLength) 
+		: base($"Właściwość \"{propertyName}\" w zasobie \"{source.GetDescription()}\" zezwala na wprowadzenie tekstu o maksymalnej długości {maximuLength} znaków.") 
+	{ }
+}
---
+++ Infrastructure/Models/Exceptions/RequiredValueException.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Common.Helpers;
+using Infrastructure.Models.Enums;
+
+namespace Infrastructure.Models.Exceptions;
+
+public class RequiredValueException : Exception
+{
+	public RequiredValueException(AppSource source, string propertyName) 
+		: base($"Właściwość \"{propertyName}\" w zasobach \"{source.GetDescription()}\" jest wymagana.")
+	{ }
+}
