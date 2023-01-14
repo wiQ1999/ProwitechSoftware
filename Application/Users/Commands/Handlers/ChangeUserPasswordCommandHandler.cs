f3f14d662ff3e4ae10d3b63d1fb69cf5e0dd33ee
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-14 23:30:42 +0100

    Dodanie hashowania do ChangeUserPasswordCommandHandler

--- Application/Users/Commands/Handlers/ChangeUserPasswordCommandHandler.cs
+++ Application/Users/Commands/Handlers/ChangeUserPasswordCommandHandler.cs
@@ -1,6 +1,10 @@
 ﻿using Application.Users.Commands.Requests;
 using Infrastructure.Interfaces.UnitOfWork;
+using Infrastructure.Models.Domain;
+using Infrastructure.Models.Enums;
+using Infrastructure.Models.Exceptions;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Users.Commands.Handlers;
 
@@ -15,9 +19,17 @@ public class ChangeUserPasswordCommandHandler : IRequestHandler<ChangeUserPasswo
 
     public async Task<Unit> Handle(ChangeUserPasswordCommand request, CancellationToken cancellationToken)
     {
+        if (string.IsNullOrWhiteSpace(request.Password))
+            throw new RequiredValueException(AppSource.Users, nameof(request.Password));
+
         var user = await _unitOfWork.UsersRepository.GetByIdAsync(request.Id, cancellationToken);
 
-        user.Password = request.Password;
+        var hashed = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+        if (hashed == null)
+            throw new Exception("Błąd podczas przetwarzania hasła użytkownika.");
+
+        user.Password = hashed;
 
         await _unitOfWork.UsersRepository.UpdateAsync(user, cancellationToken);
 
