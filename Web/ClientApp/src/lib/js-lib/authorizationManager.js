84830a82efe42d1a52fdb6b834ef3a02644874d9
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-18 23:10:32 +0100

    testy obsługi daty wygaśnięcia

--- Web/ClientApp/src/lib/js-lib/authorizationManager.js
+++ Web/ClientApp/src/lib/js-lib/authorizationManager.js
@@ -6,8 +6,14 @@ export function setToken(token) {
     console.log(decode)
 
     if (decode.exp === undefined) return
+    console.log(decode.exp)
 
-    Cookies.set('token', token, { expires: decode.exp });
+    let date = new Date(1970, 0, 1);
+    date.setSeconds(decode.exp)
+
+    console.log(date)
+
+    Cookies.set('token', token, { expires: date });
 }
 
 export function getToken() {
