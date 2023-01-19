275b8a600526826d9674148374ee568355df28a1
hannahfili <hannahfilipowska@gmail.com>
2023-01-19 10:21:49 +0100

    sprawdzenie jak zrobić get properties without protocols for specific task

--- Application/InspectionTasks/Queries/Handlers/GetInspectionTaskByIdQueryHandler.cs
+++ Application/InspectionTasks/Queries/Handlers/GetInspectionTaskByIdQueryHandler.cs
@@ -3,9 +3,11 @@ using Application.InspectionTasks.Queries.Requests;
 using AutoMapper;
 using Infrastructure.Interfaces.Repositories;
 using Infrastructure.Interfaces.UnitOfWork;
+using Infrastructure.Models.Domain;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -27,6 +29,18 @@ namespace Application.InspectionTasks.Queries.Handlers
         public async Task<InspectionTaskByIdDTO> Handle(GetInspectionTaskByIdQuery request, CancellationToken cancellationToken)
         {
             var iT=await _unitOfWork.InspectionTaskRepository.GetAsync(request.Id, cancellationToken);
+
+            //TODO get properties without protocols
+            //var building = await _unitOfWork.BuildingRepository.GetAsync(iT.BuildingId, cancellationToken);
+            //var buildingProperties = building.Properties;
+
+            //ICollection<RealProperty> propertiesWithProtocols = new Collection<RealProperty>();
+
+            //foreach(var prot in iT.InspectionProtocols)
+            //{
+            //    propertiesWithProtocols.Add(prot.InspectedProperty);
+            //}
+            //var propertiesWithoutProtocols = buildingProperties.Except(propertiesWithProtocols);
             var mapped= _mapper.Map<InspectionTaskByIdDTO>(iT);
             return mapped;
         }
