a28fe22c2c541accffe977943bb11eb354a97344
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-29 14:29:56 +0100

    Implementacja edytowalnego formularza w szczegółach roli

--- Web/ClientApp/src/routes/roles/[slug]/details/+page.svelte
+++ Web/ClientApp/src/routes/roles/[slug]/details/+page.svelte
@@ -3,6 +3,7 @@
     import { onMount } from "svelte";
     import { page } from "$app/stores";
     import { getRoleById, putRole } from "$lib/stores/Roles";
+    import BaseEditableForm from "$lib/components/base/BaseEditableForm.svelte";
 
     let baseRole = {};
     let role = {};
@@ -15,55 +16,36 @@
         formNameStore.update(() => role.name ?? "");
     });
 
-    async function submitHandler() {
+    async function onSubmitHandler() {
         if (checkIfRoleChanged()) {
             await putRole($page.params.slug, role);
             baseRole = { ...role };
         }
-        changeEditingStatus();
+
+        isEditing = false;
     }
 
     function checkIfRoleChanged() {
         return !(role.id === baseRole.id && role.name === baseRole.name);
     }
 
-    function editHandler() {
-        changeEditingStatus();
-    }
-
-    function changeEditingStatus() {
-        isEditing = !isEditing;
+    function onEditingStopHandler() {
+        role = { ...baseRole };
     }
 </script>
 
-<form
-    on:submit|preventDefault={submitHandler}
-    class="w-1/2 my-[10px] mx-auto py-3 px-5 bg-[#f4f7f8] rounded-lg text-center"
+<BaseEditableForm
+    bind:isEditing
+    formName="Szczegóły roli"
+    {onEditingStopHandler}
+    {onSubmitHandler}
 >
-    <fieldset class="border-none">
-        <legend class="font-bold text-lg py-5">Szczegóły roli</legend>
-
-        <label for="role.name" class="block">Nazwa</label>
-        <input
-            type="text"
-            bind:value={role.name}
-            disabled={!isEditing}
-            required
-            class="text-base h-auto mb-8 outline-0 p-[15px] w-[100%] bg-[#e8eeef] border-2 focus:border-[#0078c8] disabled:text-[#8a97a9]"
-        />
-    </fieldset>
-
-    {#if isEditing}
-        <button
-            type="submit"
-            class="w-[100%] border-2 border-[#0078c8] p-2 mb-5 hover:bg-blue-400"
-            >Zakończ edycję
-        </button>
-    {:else}
-        <button
-            on:click|preventDefault={editHandler}
-            class="w-[100%] border-2 border-[#0078c8] p-2 mb-5 hover:bg-blue-400"
-            >Edytuj
-        </button>
-    {/if}
-</form>
+    <label for="role.name" class="block">Nazwa</label>
+    <input
+        type="text"
+        bind:value={role.name}
+        disabled={!isEditing}
+        required
+        class="text-base h-auto mb-8 outline-0 p-[15px] w-[100%] bg-[#e8eeef] border-2 focus:border-[#0078c8] disabled:text-[#8a97a9]"
+    />
+</BaseEditableForm>
