ef3f6c05a0f9bdbd845805a373490c495599c010
hannahfili <hannahfilipowska@gmail.com>
2023-03-01 08:24:45 +0100

    usunięcie tekstu ze strony main

--- Web/ClientApp/src/routes/(app)/+page.svelte
+++ Web/ClientApp/src/routes/(app)/+page.svelte
@@ -1,43 +1,5 @@
 <main>
   <slot>
-    <div class="text-lg font-sans tracking-wide p-10 mx-10 text-justify">
-      W filmie Sława młody mężczyzna jest znany jako wykonawca sztuki na
-      Broadwayu. Jest u progu sławy, ale wydaje się, że nie ma pojęcia, jak się
-      tam dostać. Próbuje zrozumieć, co to znaczy być gwiazdą. W końcu zdaje
-      sobie sprawę, że ma talent i ambicję – ale musi skutecznie wykorzystać oba
-      dary, aby osiągnąć swój cel. Talent i ambicja są ze sobą ściśle powiązane
-      i ważne jest, aby ludzie to zrozumieli. Słowo „talent” stało się modne w
-      naszym społeczeństwie. Każdy chce mieć talent, ale wiele osób nie wie, co
-      to znaczy. Niektórzy uważają, że talent jest naturalną zdolnością, z którą
-      się urodziłeś. Inni uważają, że talent to coś, czego uczy się od
-      najmłodszych lat. Tak czy inaczej, ludzie myślą, że talent może pomóc w
-      osiągnięciu celów. Jednak talent jest przydatny tylko wtedy, gdy go
-      używasz. Podstawowym problemem związanym z talentem jest to, że może on
-      wprowadzać w błąd. Ktoś może mieć talent do śpiewania, ale bez
-      odpowiedniego szkolenia prawdopodobnie zawiedzie. Ambicja, która kieruje
-      Twój talent ku właściwym celom, jest niezbędna do osiągnięcia sukcesu.
-      George Washington był utalentowanym dowódcą wojskowym, który wziął na
-      siebie ciężar poprowadzenia Armii Kontynentalnej do zwycięstwa. Waszyngton
-      nie miał naturalnej charyzmy; nie był naturalnym przywódcą, jak jego
-      mentor, Patrick Henry. Ambicja Waszyngtona skierowała jego talent we
-      właściwym kierunku i doprowadziła go do zwycięstwa w wojnie o
-      niepodległość. Z kolei sukces Waszyngtonu jako lidera zainspirował wielu
-      Amerykanów do walki również o swoje prawa. George Washington wcześnie
-      nauczył się, że ambicja może pomóc w spełnieniu marzeń. Ślepa ambicja może
-      sprowadzić cię na złą drogę, jeśli nie wiesz, co robisz. Wiele osób o
-      wielkim potencjale spędzi całe życie, realizując swoje marzenia, nie
-      zdając sobie sprawy ze swojego pełnego potencjału. Wiele osób ma dobre
-      intencje, gdy wyrusza w ambitną podróż, ale brakuje im wiedzy lub
-      dyscypliny, aby odnieść sukces. Ktoś może mieć imponującą listę osiągnięć,
-      ale to nie czyni go sukcesem, jeśli nie stosuje tych darów we właściwy
-      sposób. Utalentowana osoba może zrujnować swoje dary, będąc mało ambitnym
-      i nieostrożnym w odniesieniu do swoich sukcesów. Ważne jest, aby ludzie
-      zrozumieli, że zarówno talent, jak i ambicja są kluczowe dla osiągnięcia
-      sukcesu. Bez talentu człowiek nie może wykorzystać swojej ambicji do
-      osiągnięcia sukcesu; bez ambicji człowiek nie może skierować swoich
-      talentów na cel, który chce osiągnąć. Zarówno talenty, jak i ambicje
-      powinny być wykorzystywane mądrze i skutecznie, jeśli ludzie chcą odnieść
-      sukces w życiu.
-    </div>
+    <div class="text-lg font-sans tracking-wide p-10 mx-10 text-justify" />
   </slot>
 </main>
